3593946cda49c68daacf6893edf2cb5ce8b5c2e8
Nitin Singh <vortechcorp@gmail.com>
2023-05-16 23:52:56 +0530

    implemeted postcache, postWorker, postService and postQueue

---
+++ src/shared/services/db/post.service.ts
@@ -0,0 +1,16 @@
+import { UpdateQuery } from 'mongoose';
+
+import { IPostDocument } from '@post/interfaces/post.interface';
+import { PostModel } from '@post/models/post.schema';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { UserModel } from '@user/models/user.schema';
+
+class PostService {
+  public async addPostToDB(userId: string, createdPost: IPostDocument): Promise<void> {
+    const post: Promise<IPostDocument> = PostModel.create(createdPost);
+    const user: UpdateQuery<IUserDocument> = UserModel.updateOne({ _id: userId }, { $inc: { postsCount: 1 } });
+    await Promise.all([post, user]);
+  };
+};
+
+export const postService: PostService = new PostService();
