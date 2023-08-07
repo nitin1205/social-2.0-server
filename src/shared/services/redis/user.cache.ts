edb8617307fed160543608878da83b72e4f3b07d
Nitin Singh <vortechcorp@gmail.com>
2023-08-07 19:01:20 +0530

    added image interfaces, models, schemes and added a method in user cache to update single user in cache

--- src/shared/services/redis/user.cache.ts
+++ src/shared/services/redis/user.cache.ts
@@ -1,13 +1,16 @@
 import Logger from 'bunyan';
 
 import { BaseCache } from '@service/redis/base.cache';
-import { IUserDocument } from '@user/interfaces/user.interface';
+import { INotificationSettings, ISocialLinks, IUserDocument } from '@user/interfaces/user.interface';
 import { config } from '@root/config';
 import { ServerError } from '@global/helpers/error-handler';
 import { Helpers } from '@global/helpers/helpers';
 
 
 const log: Logger = config.createLogger('userCache');
+
+type UserItem = string | ISocialLinks | INotificationSettings;
+
 
 export class UserCache extends BaseCache {
   constructor() {
@@ -120,6 +123,22 @@ export class UserCache extends BaseCache {
     } catch (error) {
       log.error(error);
       throw new ServerError('Server Error. Try again.');
+    };
+  };
+
+  public async updateSingleUserItemInCache(userId: string, prop: string, value: UserItem): Promise<IUserDocument | null> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      };
+      const dataToSave: string[] = [`${prop}`, JSON.stringify(value)];
+      await this.client.HSET(`users:${userId}`, dataToSave);
+      const response: IUserDocument = await this.getUserFromCache(userId) as IUserDocument;
+
+      return response;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error.Try again.');
     };
   };
 };
