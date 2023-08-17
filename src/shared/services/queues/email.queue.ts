189e7ad2438bfaaf0f7863ce639a28b367c6262c
Nitin Singh <vortechcorp@gmail.com>
2023-08-17 22:18:15 +0530

    added addchat controller class

--- src/shared/services/queues/email.queue.ts
+++ src/shared/services/queues/email.queue.ts
@@ -9,7 +9,7 @@ class EmailQueue extends BaseQueue {
     this.processJob('commentsEmail', 5, emailWorker.addNotificationEmail);
     this.processJob('FollowersEmail', 5, emailWorker.addNotificationEmail);
     this.processJob('reactionsEmail', 5, emailWorker.addNotificationEmail);
-    this.processJob('directNessageEmail', 5, emailWorker.addNotificationEmail);
+    this.processJob('directMessageEmail', 5, emailWorker.addNotificationEmail);
   };
 
   public addEmailJob(name: string, data: IEmailJob): void {
